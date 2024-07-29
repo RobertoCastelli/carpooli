6c5d812098b334602ae0e25d0438e33523fe4502
RobertoCastelli <roberto.castelli.teal@gmail.com>
2024-07-29 10:35:25 +0200

    tolto bg e sistemato header

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,6 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <footer>
-      <h5>politecnico di milano 1863</h5>
       <h5>
         <span className="footer-agis">a.g.i.s.</span> 2024 v.01.07
       </h5>
